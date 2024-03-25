389edb5deac22eff6c9d1274bfc27b47136183db
Łukasz Prokulski <prokulski@gmail.com>
2024-03-25 11:13:47 +0100

    printy

--- dzien_01/main.py
+++ dzien_01/main.py
@@ -1,1 +1,64 @@
-print("hello world")
+# print("hello world")
+# toJestJakasZmienna - tak nie robimy
+# to_jest_jakas_zmienna
+
+# a = 1
+# b = a + 4
+# print(a + 1, b -1)
+
+
+# a = "nowe"
+# a = "inne"
+# print(a + "tekst")
+
+# print("ciapki podwójne", 'ciapki pojedyncze')
+
+
+# zmienna_int = 10
+# zmienna_int_zmiana = zmienna_int / 3
+# zmienna_float = 1.234
+# zmienna_bool = True
+# zmienna_string = "napis"
+# print(zmienna_int, zmienna_int_zmiana, zmienna_float, zmienna_bool, zmienna_string)
+
+# to będzie błąd:
+# print(zmienna_float + zmienna_string)
+
+
+# zmienna_int: int = 10
+# zmienna_int_zmiana: float
+# zmienna_int_zmiana  = zmienna_int / 3
+# zmienna_float: float= 1.234
+# zmienna_bool: bool = True
+# zmienna_string: str = "napis"
+# # print(zmienna_int, zmienna_int_zmiana, zmienna_float, zmienna_bool, zmienna_string)
+
+# print("Wypisanie zmiennych z .format():")
+# print(
+#     "tu będzie int: {}\nzmienna_string: {}\nzmienna_int_zmiana: {}\n".format(
+#         zmienna_int, zmienna_int_zmiana, zmienna_string
+#     )
+# )
+
+# print("Wypisanie zmiennych z f-string:")
+# print(f"tu będzie int: {zmienna_int}\nzmienna_string: {zmienna_string}\nzmienna_int_zmiana: {zmienna_int_zmiana}\n")
+
+
+# zmienna_float: float = 1.23456789
+# # # print(f'tekst "egege" ')
+# # print(f"{zmienna_float:>50}")
+# # print(f"{zmienna_float:.0f}")
+
+# print(f"{zmienna_float*50:.1f}")
+
+# imie = "Łukasz"
+# print("Twoje imię to {imie}, życzę Ci miłego dnia!")
+# print(imie)
+# print("Twoje imię to", imie, ", życzę Ci miłego dnia!")
+# print("Twoje imię to "+imie, ", życzę Ci miłego dnia!")
+# wynik = f"Twoje imię to {imie}, życzę Ci miłego dnia!"
+# print(wynik)
+
+
+# zmienna = input("podaj jakiś ciąg: ")
+# print(zmienna)
