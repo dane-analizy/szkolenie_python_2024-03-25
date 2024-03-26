cccb5243870f1fd8588ec99f46eaf544c3878583
Łukasz Prokulski <prokulski@gmail.com>
2024-03-26 09:06:16 +0100

    zadanie

--- dzien_02/main.py
+++ dzien_02/main.py
@@ -6,3 +6,16 @@ szukane_slowo = "szlachta"
 for numer_linii, linia in enumerate(open(nazwa_pliku, "r", encoding="utf-8"), start=1):
     if szukane_slowo.lower() in linia.lower():
         print(f"{numer_linii:>6}: {linia.strip()}")
+
+
+# print("=" * 80)
+
+# # ZADANIE
+
+# """
+# Napisz program który będzie pobierał nazwę pliku z kodem w Pythonie.
+# Program będzie wyświetlał wszystkie linie które **nie** są komentarzami i nie są puste, razem z numerem linii
+# """
+
+napis = "Ala ma kota"
+print(napis[0])
