f8f6f227d6f21564ed624976b2cab02fb3855368
Łukasz Prokulski <prokulski@gmail.com>
2024-03-26 09:20:03 +0100

    rozwiązanie zadania

--- dzien_02/main.py
+++ dzien_02/main.py
@@ -1,21 +1,27 @@
 # przeszukiwanie w pliku - komenda grep
 
-nazwa_pliku = "pan-tadeusz.txt"
-szukane_slowo = "szlachta"
+# nazwa_pliku = "pan-tadeusz.txt"
+# szukane_slowo = "szlachta"
 
-for numer_linii, linia in enumerate(open(nazwa_pliku, "r", encoding="utf-8"), start=1):
-    if szukane_slowo.lower() in linia.lower():
-        print(f"{numer_linii:>6}: {linia.strip()}")
+# for numer_linii, linia in enumerate(open(nazwa_pliku, "r", encoding="utf-8"), start=1):
+#     if szukane_slowo.lower() in linia.lower():
+#         print(f"{numer_linii:>6}: {linia.strip()}")
 
 
 # print("=" * 80)
 
-# # ZADANIE
+# # # ZADANIE
 
-# """
-# Napisz program który będzie pobierał nazwę pliku z kodem w Pythonie.
-# Program będzie wyświetlał wszystkie linie które **nie** są komentarzami i nie są puste, razem z numerem linii
-# """
+# # """
+# # Napisz program który będzie pobierał nazwę pliku z kodem w Pythonie.
+# # Program będzie wyświetlał wszystkie linie które **nie** są komentarzami i nie są puste, razem z numerem linii
+# # """
 
-napis = "Ala ma kota"
-print(napis[0])
+# # napis = "Ala ma kota"
+# # print(napis[0])
+
+
+# nazwa_pliku = input("Podaj nazwę pliku pythona: ")
+# for linia in open(nazwa_pliku, "r", encoding="utf-8"):
+#     if len(linia.strip()) > 0 and linia[0] != '#':
+#         print(linia.rstrip())
