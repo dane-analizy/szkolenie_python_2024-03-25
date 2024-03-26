24429c6c44f1df8cf7f88f487a6461ca9cc3d5f2
Łukasz Prokulski <prokulski@gmail.com>
2024-03-26 10:08:46 +0100

    iterowanie po liście

--- dzien_02/main.py
+++ dzien_02/main.py
@@ -30,10 +30,39 @@
 
 
 # zdefiniowanie listy
-lista = [1, 2, 3, 3.14, 2.78, 21.37, "abc", "def"]
-print(lista)
-print(type(lista))
+# lista = [1, 2, 3, 3.14, 2.78, 21.37, "abc", "def"]
+# print(lista)
+# print(type(lista))
 
-# wyswietlenie typów kolejnych elementów na liście
-for element in lista:
-    print(element, type(element))
+# # wyswietlenie typów kolejnych elementów na liście
+# for element in lista:
+#     print(element, type(element))
+
+# lista = [1, 2, 3, 3.14, 2.78, 21.37, "abc", "def"]
+
+# for licznik, element in enumerate(lista):
+#     print(licznik, element, type(element))
+#     print(f"Pokazuję {licznik} element z listy, który ma wartość: {element:>10} | {type(element)} |")
+
+
+# from datetime import datetime
+
+# now = datetime.now()
+# print(f"{now:%Y-%m-%d-%H}")
+
+# lista = [1, 2, 3, 3.14, 2.78, 21.37, "abc", "def"]
+# print(lista[1])  # oczekujemy wartości 2
+# print(lista[0:4])  # oczekujemy wartości [1, 2, 3, 3.14]
+
+# print(lista[:4]) # od początku do 4
+# print(lista[4:]) # od 4 do końca
+
+# print(lista[2:6:2]) # od 2 do 6 co drugi
+
+# # range(start, stop, krok)
+# print(lista[6:4])
+# print(lista[6:4:-1]) # od 6 do 4 idąc od tyłu
+
+# print(lista[::-1]) # wszystkie w odwrotnej kolejności
+
+# print(lista[::-2]) # co drugi od końca
