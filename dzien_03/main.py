3c27f20a4641713df495255d8a00759b5626491c
Łukasz Prokulski <prokulski@gmail.com>
2024-03-27 10:14:41 +0100

    funkcje - return i paramtery

--- dzien_03/main.py
+++ dzien_03/main.py
@@ -56,7 +56,96 @@ od 0 do 20
 
 
 # ZADANIE
+
 """
-Napisz funkcję która na konsoli wypisze "Pozdrawiam IMIE", gdzie imie będzie podanym przez użytkownia ciągiem znaków (w ramach input).
+Napisz funkcję która na konsoli wypisze "Pozdrawiam IMIE", gdzie imie będzie podanym przez użytkownika ciągiem znaków (w ramach input).
 Niech program zapyta o imię 2 razy i wywoła 2 razy przygotowaną funkcję.
+"""
+
+# nazwisko = "kowalski"
+
+# def pozdrow(imie):
+#     nazwisko = "nowak"
+#     print(f"Cześć {imie} {nazwisko}!")
+#     imie = "nadpisałem imię"
+#     print(imie)
+
+
+# for _ in range(2):
+#     imie = input("Kogo chcesz pozdrowić?")
+#     pozdrow(imie)
+#     print(imie, nazwisko)
+
+
+# def pozdrow(imie):
+#     imie = input("1.Podaj imię")
+#     print(f"Cześć {imie}!")
+
+
+# pozdrow("Łukasz")
+
+
+# def pozdrow(imie, wiek, miasto):
+#     print(f"Cześć {imie}! Widzę, że masz {wiek} lat i mieszkasz w {miasto}")
+
+# pozdrow("Łukasz", miasto="Warszawa", wiek=35)
+
+
+# def przygotuj_pozdrowienie(imie, wiek, miasto):
+#     pozdrowienie = f"Cześć {imie}! Widzę, że masz {wiek} lat i mieszkasz w {miasto}"
+#     return pozdrowienie
+
+# ciag_znakow = przygotuj_pozdrowienie("Ania", 25, "Kraków")
+# print(ciag_znakow)
+# print(ciag_znakow.split(" "))
+
+
+# def przygotuj_pozdrowienie(imie, wiek, miasto):
+#     pozdrowienie = f"Cześć {imie}! Widzę, że masz {wiek} lat i mieszkasz w {miasto}"
+#     return pozdrowienie
+
+
+# def rozbij_na_slowa(ciag):
+#     print(ciag.split(" "))
+
+
+# def pozdrow_w_kawalkach(imie, wiek, miasto):
+#     ciag_znakow = przygotuj_pozdrowienie(imie, wiek, miasto)
+#     rozbij_na_slowa(ciag_znakow)
+
+
+# pozdrow_w_kawalkach("Ania", 25, "Kraków")
+
+
+# def dzialanie(typ_dzialania, liczba_a, liczba_b):
+#     if typ_dzialania == "dodawanie":
+#         wynik = liczba_a + liczba_b
+#     elif typ_dzialania == "odejmowanie":
+#         wynik = liczba_a - liczba_b
+#     else:
+#         print("Nie znam tego działania")
+#         wynik = None
+    
+#     print("skończyłem i zwracam wynik")
+#     return wynik
+
+# wynik_dzialania = dzialanie("dodawanie", 2, 4)
+
+# # pokazanie wyniku
+# print(wynik_dzialania)
+
+
+# wynik_dzialania = dzialanie("mnożenie", 2, 4)
+
+# # pokazanie wyniku
+# print(wynik_dzialania)
+
+
+# ZADADANIE
+"""
+Napisz funkcję, która zwraca wartości:
+     - -1 jeśli podana w parametrze liczba jest ujemna
+     - +1 jesli liczba jest dodatnia
+     - 0 jeśli liczba z parametru jest zerem 
+Wynik, który zwróci funkcja wypisz na ekranie
 """
