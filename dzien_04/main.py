5ebc047eaffe590e231d6e3a48ae987914c6d552
Łukasz Prokulski <prokulski@gmail.com>
2024-03-28 13:43:13 +0100

    stworzenie tabeli i napełnienie jej przykładowymi rekordami

--- dzien_04/main.py
+++ dzien_04/main.py
@@ -337,3 +337,33 @@ Odpowiedź z API:
 
 # for d in notowania_usd_pod_4zl:
 #     print(d[0], d[1])
+
+
+"""
+create table players (
+	player_id serial primary key,
+	first_name text not null,
+	last_name text not null,
+	height numeric not null,
+	weight numeric not null
+);
+
+insert into players (first_name,last_name,height,weight)  
+values ('Jan', 'Kowalski', 1.84, 85);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Marian', 'Nowak', 1.90, 50);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Zdzisław', 'Dyrman', 1.73 ,74);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Zenon', 'Brzęczyk', 1.64, 95);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Chuck','Norris', 1.82, 78);  
+  
+insert into players (first_name,last_name,height,weight)  
+values ('Krzysztof','Jarzyna', 1.68, 70);
+
+"""
