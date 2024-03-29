8930ebdaec77e587888bf3f2faf5e2d1c816b0e4
Łukasz Prokulski <prokulski@gmail.com>
2024-03-29 12:30:24 +0100

    rozwiażanie

--- dzien_05/dowcipy.py
+++ dzien_05/dowcipy.py
@@ -1,44 +1,44 @@
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
 
-app = Flask(__name__)  # nazwa będzie wzięta z nazwy pliku pythona
-
-
-# @app.route("/zmienna")
-# def main():
-#     return render_template("zmienna_z_kodu.html", abc="inna wartosc", xyz=12.34)
+# app = Flask(__name__)  # nazwa będzie wzięta z nazwy pliku pythona
 
 
-# @app.route("/slownik")
-# def main_dict():
-#     d = {"klucz1": "napis", "klucz2": 123, "klucz3": "jestem kluczem 3"}
-#     return render_template("slownik_z_kodu.html", abc=d)
+# # @app.route("/zmienna")
+# # def main():
+# #     return render_template("zmienna_z_kodu.html", abc="inna wartosc", xyz=12.34)
 
 
-# @app.route("/lista")
-# def main_lista():
-#     d = ("klucz1", "napis", "klucz2", 123, "klucz3", "jestem kluczem 3")
-#     return render_template("lista_z_kodu.html", lista=d)
+# # @app.route("/slownik")
+# # def main_dict():
+# #     d = {"klucz1": "napis", "klucz2": 123, "klucz3": "jestem kluczem 3"}
+# #     return render_template("slownik_z_kodu.html", abc=d)
 
 
-# parametry z ? - przykładowe wywołanie: /parametry?klucz=124&inny_klucz=asbaegf
-@app.route("/parametry")
-def parametry():
-    # w request.args - słownik z parameterami podanymi w adresie; coś jak **kwargs w funkcjach
-    # jeśli chcesz dodać klucz - musisz zrobić kopię
-    moje_slownik = request.args.copy()
-    moje_slownik["domek"] = "domek jest pusty"
-    # return request.args
-    return render_template("slownik_z_kodu.html", abc=moje_slownik)
+# # @app.route("/lista")
+# # def main_lista():
+# #     d = ("klucz1", "napis", "klucz2", 123, "klucz3", "jestem kluczem 3")
+# #     return render_template("lista_z_kodu.html", lista=d)
 
 
-@app.route("/dodawanie/<a>/<b>")
-def dodawanie(a, b):
-    wynik = float(a) + float(b)
-    return str(wynik)
+# # parametry z ? - przykładowe wywołanie: /parametry?klucz=124&inny_klucz=asbaegf
+# @app.route("/parametry")
+# def parametry():
+#     # w request.args - słownik z parameterami podanymi w adresie; coś jak **kwargs w funkcjach
+#     # jeśli chcesz dodać klucz - musisz zrobić kopię
+#     moje_slownik = request.args.copy()
+#     moje_slownik["domek"] = "domek jest pusty"
+#     # return request.args
+#     return render_template("slownik_z_kodu.html", abc=moje_slownik)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# @app.route("/dodawanie/<a>/<b>")
+# def dodawanie(a, b):
+#     wynik = float(a) + float(b)
+#     return str(wynik)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 # ZADANIE
@@ -57,3 +57,28 @@ Zrób endpointy:
  - odejmujący liczby a i b, wywoływany w postaci /odejmowanie/a/b
  - mnożący liczby a i b, wywoływany w postaci /mnozenie?a=..&b=...
 """
+
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/odejmowanie/<a>/<b>")
+def odejmowanie(a, b):
+    wynik = float(a) - float(b)
+    return str(wynik)
+
+
+@app.route("/mnozenie")
+def mnozenie():
+    a = request.args.get("a")
+    b = request.args.get("b")
+    if not a or not b:
+        return "Co ty podajesz za głupoty?"
+    wynik = float(a) * float(b)
+    return str(wynik)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
