de725ea4b85f0644a5dde25015f2ad12e6c3f2ec
Łukasz Prokulski <prokulski@gmail.com>
2024-03-29 10:00:18 +0100

    html home

--- dzien_05/main.py
+++ dzien_05/main.py
@@ -16,6 +16,37 @@
 #     app.run()
 
 
+# from flask import Flask
+
+# app = Flask(__name__)  # nazwa będzie wzięta z nazwy pliku pythona
+
+
+# @app.route("/")
+# def home_page():
+#     print("uruchamiam stronę główną")
+#     return "To ja, strona główna i życzę miłego dnia"
+
+
+# @app.route("/stronapierwsza")
+# def strona_pierwsza():
+#     return "Jestem stroną pierwszą"
+
+
+# @app.route("/stronadruga")
+# def strona_druga():
+#     return "Jestem stroną drugą"
+
+
+# if __name__ == "__main__":
+#     # debug=True powoduje, że Flask przeładuje aplikację po zmianie kodu
+#     app.run(debug=True)
+
+
+# ZADANIE
+
+# Do powyższej aplikacji dodaj kilka kolejnych strony.
+
+
 from flask import Flask
 
 app = Flask(__name__)  # nazwa będzie wzięta z nazwy pliku pythona
@@ -27,16 +58,6 @@ def home_page():
     return "To ja, strona główna i życzę miłego dnia"
 
 
-@app.route("/stronapierwsza")
-def strona_pierwsza():
-    return "Jestem stroną pierwszą"
-
-
 if __name__ == "__main__":
     # debug=True powoduje, że Flask przeładuje aplikację po zmianie kodu
     app.run(debug=True)
-
-
-# ZADANIE
-
-# Do powyższej aplikacji dodaj kilka kolejnych strony.
