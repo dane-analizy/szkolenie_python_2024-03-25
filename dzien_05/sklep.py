c748645e174c40ce7a44fddea86881c1d15eda21
Łukasz Prokulski <prokulski@gmail.com>
2024-03-29 10:56:37 +0100

    szablon strony sklepu

---
+++ dzien_05/sklep.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)  # nazwa będzie wzięta z nazwy pliku pythona
+
+
+@app.route("/")
+def main():
+    return render_template("sklep_hp.html")
+
+
+@app.route("/produkty")
+def produkty():
+    return render_template("sklep_produkty.html")
+
+
+@app.route("/uslugi")
+def uslugi():
+    return render_template("sklep_uslugi.html")
+
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("sklep_kontakt.html")
+
+
+@app.route("/o_nas")
+def o_nas():
+    return render_template("sklep_onas.html")
+
+
+if __name__ == "__main__":
+    # debug=True powoduje, że Flask przeładuje aplikację po zmianie kodu
+    app.run(debug=True)
