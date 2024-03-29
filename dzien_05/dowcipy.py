654ce418bd95976bc5d97f7be37bb6728b537022
Łukasz Prokulski <prokulski@gmail.com>
2024-03-29 12:15:41 +0100

    paramtery

--- dzien_05/dowcipy.py
+++ dzien_05/dowcipy.py
@@ -1,23 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)  # nazwa będzie wzięta z nazwy pliku pythona
 
 
-@app.route("/zmienna")
-def main():
-    return render_template("zmienna_z_kodu.html", abc="inna wartosc", xyz=12.34)
+# @app.route("/zmienna")
+# def main():
+#     return render_template("zmienna_z_kodu.html", abc="inna wartosc", xyz=12.34)
 
 
-@app.route("/slownik")
-def main_dict():
-    d = {"klucz1": "napis", "klucz2": 123, "klucz3": "jestem kluczem 3"}
-    return render_template("slownik_z_kodu.html", abc=d)
+# @app.route("/slownik")
+# def main_dict():
+#     d = {"klucz1": "napis", "klucz2": 123, "klucz3": "jestem kluczem 3"}
+#     return render_template("slownik_z_kodu.html", abc=d)
 
 
-@app.route("/lista")
-def main_lista():
-    d = ("klucz1", "napis", "klucz2", 123, "klucz3", "jestem kluczem 3")
-    return render_template("lista_z_kodu.html", lista=d)
+# @app.route("/lista")
+# def main_lista():
+#     d = ("klucz1", "napis", "klucz2", 123, "klucz3", "jestem kluczem 3")
+#     return render_template("lista_z_kodu.html", lista=d)
+
+
+# parametry z ? - przykładowe wywołanie: /parametry?klucz=124&inny_klucz=asbaegf
+@app.route("/parametry")
+def parametry():
+    # w request.args - słownik z parameterami podanymi w adresie; coś jak **kwargs w funkcjach
+    # jeśli chcesz dodać klucz - musisz zrobić kopię
+    moje_slownik = request.args.copy()
+    moje_slownik["domek"] = "domek jest pusty"
+    # return request.args
+    return render_template("slownik_z_kodu.html", abc=moje_slownik)
+
+
+@app.route("/dodawanie/<a>/<b>")
+def dodawanie(a, b):
+    wynik = float(a) + float(b)
+    return str(wynik)
 
 
 if __name__ == "__main__":
