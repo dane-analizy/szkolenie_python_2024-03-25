aa3dd971f6b4d50c3d7f1251435731587611c46d
Łukasz Prokulski <prokulski@gmail.com>
2024-03-29 11:36:03 +0100

    pokazanie slownika

--- dzien_05/dowcipy.py
+++ dzien_05/dowcipy.py
@@ -5,7 +5,13 @@ app = Flask(__name__)  # nazwa będzie wzięta z nazwy pliku pythona
 
 @app.route("/zmienna")
 def main():
-    return render_template("zmienna_z_kodu.html", abc="inna wartosc")
+    return render_template("zmienna_z_kodu.html", abc="inna wartosc", xyz=12.34)
+
+
+@app.route("/slownik")
+def main_dict():
+    d = {"klucz1": "napis", "klucz2": 123, "klucz3": "jestem kluczem 3"}
+    return render_template("slownik_z_kodu.html", abc=d)
 
 
 if __name__ == "__main__":
