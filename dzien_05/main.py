b7118fe789817542936ea7d8d413fb3611f4b53f
Łukasz Prokulski <prokulski@gmail.com>
2024-03-29 09:42:26 +0100

    flask - strona główna

--- dzien_05/main.py
+++ dzien_05/main.py
@@ -5,14 +5,27 @@
 
 # pip install flask
 
+# from flask import Flask
+
+# # app = Flask("nasza apka") # w nawiasie nazwa naszej aplikacji
+# app = Flask(__name__) # nazwa będzie wzięta z nazwy pliku pythona
+
+# # print(app)
+
+# if __name__ == '__main__':
+#     app.run()
+
+
 from flask import Flask
 
-# app = Flask("nasza apka") # w nawiasie nazwa naszej aplikacji
-app = Flask(__name__) # nazwa będzie wzięta z nazwy pliku pythona
+app = Flask(__name__)  # nazwa będzie wzięta z nazwy pliku pythona
 
 
-# print(app)
+@app.route("/")
+def home_page():
+    print("uruchamiam stronę główną")
+    return "To ja, strona główna i życzę miłego dnia"
 
-if __name__ == '__main__':
-    app.run()
-    
+
+if __name__ == "__main__":
+    app.run(debug=True)
